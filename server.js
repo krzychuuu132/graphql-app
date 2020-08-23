cbfaaa2fa118580ce206e2a0fa53a436de70e67b
krzychuuu132 <52799848+krzychuuu132@users.noreply.github.com>
2020-08-24 00:24:51 +0200

    Connecting to DB/Creating Event model and configure database with graphql

--- server.js
+++ server.js
@@ -2,19 +2,41 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {graphqlHTTP} = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const Event = require('./models/event');
 
 const app = express();
 
 app.use(bodyParser.json());
 
+
+
+
+
 app.use('/graphql',graphqlHTTP({
     schema:buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput{
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
         type RootQuery {
-            events: [String!]!
+            events: [Event!]!
         }
 
         type  RootMutation{
-                createEvent(name: String):String
+                createEvent(eventInput: EventInput):Event
         }
 
     schema {
@@ -24,15 +46,51 @@ app.use('/graphql',graphqlHTTP({
     `),
     rootValue:{
         events:()=>{
-            return ['cooking','selling','programming']
+           return Event.find()
+            .then(events=> {
+                events.forEach(event=>event)
+            })
+            .catch(err=>{
+                throw err;
+            })
         },
         createEvent:(args)=>{
-                const eventName = args.name;
-                return eventName;
+             const { eventInput: arg }  = args;
+             
+                const event = new Event({
+                    title: arg.title,
+                    description: arg.description,
+                    price: arg.price,
+                    date: new Date(arg.date)
+                });
+                return event
+                .save()
+                .then(result=>{
+                        console.log(result);
+                        return {...result._doc};
+                    })
+                .catch(err=>{
+                        console.log(err);
+                        throw err;
+                    })
+               
+                
+                
+           
+                return event
         }
     },
     graphiql: true
 }))
 
+mongoose.connect(process.env.DB_CONNECTION ,{ useNewUrlParser: true },()=>{
+    console.log("connected to DB!")
+})
+.then(()=>{
+app.listen(process.env.PORT||3000,()=>console.log('Server has been run!'));
+})
+.catch(err=>{
+    console.log(err)
+})
 
-app.listen(process.env.PORT||3000,()=>console.log('Server has been run!'))
+
